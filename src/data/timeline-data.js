909f39eaeeec7fd474e693c6fd5aa47da8421243
Emir Özder <emir.ozder@id3.com.tr>
2024-11-07 22:16:17 +0300

    timeline image path upd

--- src/data/timeline-data.js
+++ src/data/timeline-data.js
@@ -1,32 +1,39 @@
+import image7 from "/images/timeline-data-images/eylul_ekim.jpg";
+import image1 from "/images/timeline-data-images/kasim_aralik.jpg";
+import image5 from "/images/timeline-data-images/mayis_haziran.jpg";
+import image4 from "/images/timeline-data-images/nisan.jpg";
+import image2 from "/images/timeline-data-images/ocak.jpg";
+import image3 from "/images/timeline-data-images/subat-mart.jpg";
+import image6 from "/images/timeline-data-images/temmuz.jpg";
 const timeline_data = [
   {
     title: "Kasım",
-    image: "public/images/timeline-data-images/kasim_aralik.jpg",
+    image: image1,
   },
   {
     title: "Aralık - Ocak",
-    image: "public/images/timeline-data-images/ocak.jpg",
+    image: image2,
   },
   {
     title: "Şubat - Mart",
-    image: "public/images/timeline-data-images/subat-mart.jpg",
+    image: image3,
   },
   {
     title: "Nisan",
-    image: "public/images/timeline-data-images/nisan.jpg",
+    image: image4,
     scale: "contain",
   },
   {
     title: "Mayıs - Haziran",
-    image: "public/images/timeline-data-images/mayis_haziran.jpg",
+    image: image5,
   },
   {
     title: "Temmuz",
-    image: "public/images/timeline-data-images/temmuz.jpg",
+    image: image6,
   },
   {
     title: "Ağustos - Eylül - Ekim",
-    image: "public/images/timeline-data-images/eylul_ekim.jpg",
+    image: image7,
   }
 ]
 
